1c59001289195ba25d645eb97a24cde24acfbcdc
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-07-02 05:31:27 +0200

    Separated tests for starter packs

---
+++ src/test/java/com/d20charactersheet/framework/boc/service/CharacterClassServiceStarterPackTest.kt
@@ -0,0 +1,79 @@
+package com.d20charactersheet.framework.boc.service
+
+import com.d20charactersheet.framework.boc.model.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class CharacterClassServiceStarterPackTest {
+
+    private val underTest = CharacterCreatorServiceImpl()
+
+    @Test
+    fun getStarterPack_fighter_getCompleteSelectionForFighter() {
+        // arrange
+        val allWeaopns: List<Weapon> = mock()
+        val allArmor: List<Armor> = mock()
+        val itemService: ItemService = mock()
+        whenever(itemService.getItemById(1, allWeaopns)).thenReturn(Weapon().apply { name = "Longbow" })
+        whenever(itemService.getItemById(2, allWeaopns)).thenReturn(Weapon().apply { name = "Arrow" })
+        whenever(itemService.getItemById(3, allWeaopns)).thenReturn(Weapon().apply { name = "Crossbow, light" })
+        whenever(itemService.getItemById(4, allWeaopns)).thenReturn(Weapon().apply { name = "Bolt" })
+        whenever(itemService.getItemById(5, allWeaopns)).thenReturn(Weapon().apply { name = "Handaxe" })
+
+        whenever(itemService.getItemById(1, allArmor)).thenReturn(Armor().apply { name = "Chain mail" })
+        whenever(itemService.getItemById(2, allArmor)).thenReturn(Armor().apply { name = "Leather" })
+        whenever(itemService.getItemById(3, allArmor)).thenReturn(Armor().apply { name = "Shield" })
+
+        whenever(itemService.filterWeaponsByType(WeaponType.MARTIAL)).thenReturn(
+            listOf(
+                Weapon().apply { name = "Longsword" },
+                Weapon().apply { name = "Battleaxe" },
+                Weapon().apply { name = "Bastardsword" },
+                Weapon().apply { name = "Two-Handed sword" },
+                Weapon().apply { name = "Helberd" },
+            )
+        )
+
+        // act
+        val starterPack: StarterPack = underTest.getStarterPack(CharacterClass(), itemService, allWeaopns, allArmor)
+
+        // assert
+        assertThat(starterPack.selectionBoxes).hasSize(5)
+
+        assertThat(starterPack.selectionBoxes[0].name).isEqualTo("Armor")
+        assertThat(starterPack.selectionBoxes[0].options).hasSize(2)
+        assertThat(starterPack.selectionBoxes[0].options[0].getTitle()).isEqualTo("Chain mail")
+        assertThat(starterPack.selectionBoxes[0].options[1].getTitle()).isEqualTo("Leather, Longbow, Arrow (20)")
+
+        assertThat(starterPack.selectionBoxes[1].name).isEqualTo("Primary Hand")
+        assertThat(starterPack.selectionBoxes[1].options).hasSize(5)
+        assertThat(starterPack.selectionBoxes[1].options[0].getTitle()).isEqualTo("Longsword")
+        assertThat(starterPack.selectionBoxes[1].options[1].getTitle()).isEqualTo("Battleaxe")
+        assertThat(starterPack.selectionBoxes[1].options[2].getTitle()).isEqualTo("Bastardsword")
+        assertThat(starterPack.selectionBoxes[1].options[3].getTitle()).isEqualTo("Two-Handed sword")
+        assertThat(starterPack.selectionBoxes[1].options[4].getTitle()).isEqualTo("Helberd")
+
+        assertThat(starterPack.selectionBoxes[2].name).isEqualTo("Secondary Hand")
+        assertThat(starterPack.selectionBoxes[2].options).hasSize(6)
+        assertThat(starterPack.selectionBoxes[2].options[0].getTitle()).isEqualTo("Longsword")
+        assertThat(starterPack.selectionBoxes[2].options[1].getTitle()).isEqualTo("Battleaxe")
+        assertThat(starterPack.selectionBoxes[2].options[2].getTitle()).isEqualTo("Bastardsword")
+        assertThat(starterPack.selectionBoxes[2].options[3].getTitle()).isEqualTo("Two-Handed sword")
+        assertThat(starterPack.selectionBoxes[2].options[4].getTitle()).isEqualTo("Helberd")
+        assertThat(starterPack.selectionBoxes[2].options[5].getTitle()).isEqualTo("Shield")
+
+        assertThat(starterPack.selectionBoxes[3].name).isEqualTo("Range Weapon")
+        assertThat(starterPack.selectionBoxes[3].options).hasSize(2)
+        assertThat(starterPack.selectionBoxes[3].options[0].getTitle()).isEqualTo("Crossbow, light, Bolt (20)")
+        assertThat(starterPack.selectionBoxes[3].options[1].getTitle()).isEqualTo("Handaxe (2)")
+
+        assertThat(starterPack.selectionBoxes[4].name).isEqualTo("Equipment Pack")
+        assertThat(starterPack.selectionBoxes[4].options).hasSize(2)
+        assertThat(starterPack.selectionBoxes[4].options[0].getTitle()).isEqualTo("Dungeoneer's Pack")
+        assertThat(starterPack.selectionBoxes[4].options[1].getTitle()).isEqualTo("Explorer's Pack")
+
+    }
+
+}
