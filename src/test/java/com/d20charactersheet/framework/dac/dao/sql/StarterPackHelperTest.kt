df5d82acd96ac4a47ec91cfa01b2f80f6cdf3f76
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-07-05 08:45:50 +0200

    Simplified test

--- src/test/java/com/d20charactersheet/framework/dac/dao/sql/StarterPackHelperTest.kt
+++ src/test/java/com/d20charactersheet/framework/dac/dao/sql/StarterPackHelperTest.kt
@@ -1,6 +1,5 @@
 package com.d20charactersheet.framework.dac.dao.sql
 
-import com.d20charactersheet.framework.boc.model.StarterPackQuery
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -9,14 +8,12 @@ class StarterPackHelperTest {
 
     @Test
     fun getStarterBoxOptions() {
-        // arrange
-        val startPackQueries: List<StarterPackQuery> = listOf()
 
         // act
-        val starterBoxOptions = StarterPackHelper().getStarterBoxOptions(startPackQueries)
+        val starterBoxOptions = StarterPackHelper().getStarterBoxOptions(listOf())
 
         // assert
-        assertThat(startPackQueries).isEmpty()
+        assertThat(starterBoxOptions).isEmpty()
     }
 
 }
