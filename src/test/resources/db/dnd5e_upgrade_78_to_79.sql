2f2f4bc7c129fc090c068b7f7657fa3a4dee32b9
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-12-20 05:27:06 +0100

    Fixed updated script to remove old save columns

--- src/test/resources/db/dnd5e_upgrade_78_to_79.sql
+++ src/test/resources/db/dnd5e_upgrade_78_to_79.sql
@@ -4,22 +4,56 @@ DROP TABLE save;
 UPDATE weapon SET weight = 0.1 WHERE id = 16;
 
 
-ALTER TABLE charakter ADD COLUMN save_str_misc_mod INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE charakter ADD COLUMN save_dex_misc_mod INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE charakter ADD COLUMN save_con_misc_mod INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE charakter ADD COLUMN save_int_misc_mod INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE charakter ADD COLUMN save_wis_misc_mod INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE charakter ADD COLUMN save_cha_misc_mod INTEGER NOT NULL DEFAULT 0;
+CREATE TABLE charakter_new
+(id 			      INTEGER,
+ player 		    VARCHAR(30) NOT NULL,
+ name 			    VARCHAR(30) NOT NULL,
+ race_id       	INTEGER NOT NULL,
+ sex_id         INTEGER NOT NULL,
+ alignment_id  	INTEGER NOT NULL,
+ xp_table_id    INTEGER NOT NULL DEFAULT 1,
+ experience 	  INTEGER NOT NULL,
+ strength   	  INTEGER NOT NULL,
+ dexterity  	  INTEGER NOT NULL,
+ constitution 	INTEGER NOT NULL,
+ intelligence	  INTEGER NOT NULL,
+ wisdom			    INTEGER NOT NULL,
+ charisma		    INTEGER NOT NULL,
+ hitpoints		  INTEGER NOT NULL,
+ max_hitpoints	INTEGER NOT NULL,
+ armor      	  INTEGER NOT NULL,
+ ini_misc_mod   INTEGER NOT NULL,
+ cmb_mod    INTEGER NOT NULL DEFAULT 0,
+ cmd_mod    INTEGER NOT NULL DEFAULT 0,
+ save_str_misc_mod  INTEGER NOT NULL DEFAULT 0,
+ save_dex_misc_mod  INTEGER NOT NULL DEFAULT 0,
+ save_con_misc_mod  INTEGER NOT NULL DEFAULT 0,
+ save_int_misc_mod  INTEGER NOT NULL DEFAULT 0,
+ save_wis_misc_mod  INTEGER NOT NULL DEFAULT 0,
+ save_cha_misc_mod  INTEGER NOT NULL DEFAULT 0,
+ image_id       INTEGER,
+ thumb_image_id INTEGER,
+ platinum       INTEGER NOT NULL DEFAULT 0,
+ gold           INTEGER NOT NULL DEFAULT 0,
+ silver         INTEGER NOT NULL DEFAULT 0,
+ copper         INTEGER NOT NULL DEFAULT 0,
+ PRIMARY KEY (id),
+ FOREIGN KEY (race_id) REFERENCES race(id) ON DELETE CASCADE,
+ FOREIGN KEY (sex_id) REFERENCES sex(id) ON DELETE CASCADE,
+ FOREIGN KEY (alignment_id) REFERENCES alignment(id) ON DELETE CASCADE,
+ FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE CASCADE,
+ FOREIGN KEY (thumb_image_id) REFERENCES image(id) ON DELETE CASCADE
+);
 
 
-UPDATE charakter SET save_str_misc_mod = fort_misc_mod;
-UPDATE charakter SET save_dex_misc_mod = ref_misc_mod;
-UPDATE charakter SET save_con_misc_mod = will_misc_mod;
+INSERT INTO charakter_new (id, player, name, race_id, sex_id, alignment_id, xp_table_id, experience, strength, dexterity, constitution, intelligence, wisdom, charisma, hitpoints, max_hitpoints, armor, ini_misc_mod, cmb_mod, cmd_mod, save_str_misc_mod, save_dex_misc_mod, save_con_misc_mod, image_id, thumb_image_id, platinum, gold, silver, copper)
+SELECT                     id, player, name, race_id, sex_id, alignment_id, xp_table_id, experience, strength, dexterity, constitution, intelligence, wisdom, charisma, hitpoints, max_hitpoints, armor, ini_misc_mod, cmb_mod, cmd_mod, fort_misc_mod,     ref_misc_mod,      will_misc_mod,     image_id, thumb_image_id, platinum, gold, silver, copper FROM charakter;
 
 
-ALTER TABLE charakter DROP COLUMN fort_misc_mod;
-ALTER TABLE charakter DROP COLUMN ref_misc_mod;
-ALTER TABLE charakter DROP COLUMN will_misc_mod;
+DROP TABLE charakter;
+
+
+ALTER TABLE charakter_new RENAME TO charakter;
 
 
 UPDATE class SET saves = 5 WHERE id = 1;
